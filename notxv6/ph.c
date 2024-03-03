c9928a342edea124eb747610903f7d1ce7710dc8
TeQin-hub <2281383561@qq.com>
2024-03-03 16:19:11 +0800

    lab6-multithread-using thread

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,9 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+//add lab6-using threads
+pthread_mutex_t locks[NBUCKET];
+
 
 double
 now()
@@ -32,29 +35,37 @@ insert(int key, int value, struct entry **p, struct entry *n)
   struct entry *e = malloc(sizeof(struct entry));
   e->key = key;
   e->value = value;
-  e->next = n;
+  e->next = n;//对头指针n所指向的table链表 使用头插法
   *p = e;
 }
 
+//add lab6-using pthread
+//对这个函数的修改就是增加四个锁操作
 static 
 void put(int key, int value)
-{
+{  
   int i = key % NBUCKET;
-
+  
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
   }
-  if(e){
+  
+  if (e)
+  {
     // update the existing key.
     e->value = value;
-  } else {
-    // the new is new.
-    insert(key, value, &table[i], table[i]);
   }
-
+  else
+  {
+    // the new is new.
+    //&table[i] 提供了一个可以修改哈希表中指定位置的指针的地址，而 table[i] 提供了该指针本身，用于标识链表的头部。
+    pthread_mutex_lock(&locks[i]);  
+    insert(key, value, &table[i], table[i]);//insert的返回值会是新的链表头节点，所以只要限制单线程进入insert即可
+    pthread_mutex_unlock(&locks[i]);
+  }
 }
 
 static struct entry*
@@ -62,12 +73,10 @@ get(int key)
 {
   int i = key % NBUCKET;
 
-
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
-
   return e;
 }
 
@@ -75,10 +84,11 @@ static void *
 put_thread(void *xa)
 {
   int n = (int) (long) xa; // thread number
-  int b = NKEYS/nthread;
+  int b = NKEYS/nthread;//每个线程需要处理的key个数
 
   for (int i = 0; i < b; i++) {
     put(keys[b*n + i], n);
+    //keys[b * n + i] 表示了当前线程要处理的键。通过这种方式，每个线程可以独立地处理一部分键值对，避免了不必要的竞争条件
   }
 
   return NULL;
@@ -105,8 +115,13 @@ main(int argc, char *argv[])
   void *value;
   double t1, t0;
 
+//add lab6-using thread
+  for (int i = 0; i < NBUCKET;++i){
+    pthread_mutex_init(&locks[i], NULL); 
+  }
 
-  if (argc < 2) {
+  if (argc < 2)
+  {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
   }
@@ -121,6 +136,7 @@ main(int argc, char *argv[])
   //
   // first the puts
   //
+  //这段代码的目的是创建多个线程来并行地执行插入操作，然后等待所有线程完成，并计算插入操作的执行时间和吞吐量。
   t0 = now();
   for(int i = 0; i < nthread; i++) {
     assert(pthread_create(&tha[i], NULL, put_thread, (void *) (long) i) == 0);
